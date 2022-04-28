aa6f8b7303183dd180a7b7b77cc17bd2f7f4dd91
socioholmes <socioholmes@gmail.com>
2022-04-28 23:28:47 +0900

    initial commit

---
+++ cpptraining/cpptraining/main.cpp
@@ -0,0 +1,11 @@
+//
+//  main.cpp
+//  cpptraining
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!2\n";
+    return 0;
+}
